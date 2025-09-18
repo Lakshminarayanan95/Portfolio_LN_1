08fc51f77b3913b0fc5a9f4136de2fd9d98dd593
Lakshminarayanan95 <kylakshminarayanan@gmail.com>
2025-09-18 16:51:38 +0530

    Update next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -30,6 +28,9 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  experimental: {
+    serverActions: true,
   },
 };
 
